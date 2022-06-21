872717d9520f00b953bf066c82f6029daf64a7ba
oceanchanchiyeung <107939537+oceanchanchiyeung@users.noreply.github.com>
2022-06-21 23:13:42 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,80 @@
-<?php
+<?php require_once('config.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once('inc/header.php') ?>
+<body>
+<?php $page = isset($_GET['p']) ? $_GET['p'] : 'home';  ?>
+<?php require_once('inc/topBarNav.php') ?>
+     <?php if($_settings->chk_flashdata('success')): ?>
+      <script>
+        alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+      </script>
+<?php endif;?>
+<?php 
+    if(!file_exists($page.".php") && !is_dir($page)){
+        include '404.html';
+    }else{
+      if(is_dir($page))
+        include $page.'/index.php';
+      else
+        include $page.'.php';
+    }
+?>
+<?php require_once('inc/footer.php') ?>
 
-declare(strict_types=1);
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog   rounded-0 modal-md modal-dialog-centered" role="document">
+      <div class="modal-content  rounded-0">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal_right" role='dialog'>
+    <div class="modal-dialog  rounded-0 modal-full-height  modal-md" role="document">
+      <div class="modal-content rounded-0">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span class="fa fa-arrow-right"></span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="viewer_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+              <img src="" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="confirm_modal" role='dialog'>
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
+      </div>
+      <div class="modal-body">
+        <div id="delete_content"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" id='confirm' onclick="">Continue</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+      </div>
+    </div>
+  </div>
 
-use PhpMyAdmin\Common;
-use PhpMyAdmin\Routing;
-
-if (! defined('ROOT_PATH')) {
-    // phpcs:disable PSR1.Files.SideEffects
-    define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
-    // phpcs:enable
-}
-
-if (PHP_VERSION_ID < 70205) {
-    die('<p>PHP 7.2.5+ is required.</p><p>Currently installed version is: ' . PHP_VERSION . '</p>');
-}
-
-// phpcs:disable PSR1.Files.SideEffects
-define('PHPMYADMIN', true);
-// phpcs:enable
-
-require_once ROOT_PATH . 'libraries/constants.php';
-
-/**
- * Activate autoloader
- */
-if (! @is_readable(AUTOLOAD_FILE)) {
-    die(
-        '<p>File <samp>' . AUTOLOAD_FILE . '</samp> missing or not readable.</p>'
-        . '<p>Most likely you did not run Composer to '
-        . '<a href="https://docs.phpmyadmin.net/en/latest/setup.html#installing-from-git">'
-        . 'install library files</a>.</p>'
-    );
-}
-
-require AUTOLOAD_FILE;
-
-global $route, $containerBuilder, $request;
-
-Common::run();
-
-$dispatcher = Routing::getDispatcher();
-Routing::callControllerForRoute($request, $route, $dispatcher, $containerBuilder);
+</body>
+</html>
